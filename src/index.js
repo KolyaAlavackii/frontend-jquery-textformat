cdef111ce19dcd7ce249789295c51d0200b67eb9
KolyaAlavackii <nikolayalavackii@gmail.com>
2016-12-01 14:57:38 +0200

    Готово!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log($)
+function formatted(context) {
+  	var formattedText = context.value
+    	.trim() 
+      	.toLowerCase() 
+      	.replace(/\n+/g, " ") 
+      	.replace(/\s+/g," "); 
+    $(".message-formatted").text(formattedText);  
+}
+
+$(".message-field").blur(function() { 
+  	formatted(this)
+	});
